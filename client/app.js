4a01104e1cb0491b6ab25805ea67e27d7b6c60e5
emonsalve <edward.monsalve@integ.ro>
2016-11-29 08:18:30 -0500

    Base

--- client/app.js
+++ client/app.js
@@ -2,25 +2,44 @@ import $ from 'jquery'
 import fullCalendar from 'fullcalendar'
 
 
+class EventsManager {
+    constructor(username) {
+        this.nombreUsuario = nombreUsuario
+        this.obtenerDataInicial()
+    }
 
-$('.calendario').fullCalendar({
-    header: {
-		left: 'prev,next today',
-		center: 'title',
-		right: 'month,basicWeek,basicDay'
-	},
-	defaultDate: '2016-11-01',
-	navLinks: true,
-	editable: true,
-	eventLimit: true,
-    droppable: true,
-    eventDrop: function(ev) {
-        console.log(ev.start.format())
-    },
-    events: [
-        {
-			title: 'Long Event',
-			start: '2016-11-24'
-        }
-    ]
-})
+    obtenerDataInicial() {
+        let url = '/eventos/all'
+        $.post(url, {nombre: this.nombreUsuario}, (eventos) => {
+            this.poblarCalendario(eventos)
+        })
+    }
+
+    poblarCalendario(eventos) {
+        $('.calendario').fullCalendar({
+            header: {
+        		left: 'prev,next today',
+        		center: 'title',
+        		right: 'month,basicWeek,basicDay'
+        	},
+        	defaultDate: '2016-11-01',
+        	navLinks: true,
+        	editable: true,
+        	eventLimit: true,
+            droppable: true,
+            eventDrop: function(ev) {
+                actualizarEvento(ev)
+            },
+            events: eventos
+        })
+    }
+
+    actualizarEvento(evento) {
+        let eventId = evento.eventId,
+            title = evento.title,
+            start = evento.start
+
+        let url = '/eventos/actualizar'
+
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,9 +8,6 @@ const app = express()
 const Server = http.createServer(app)
 const port = 3000
 
-
 app.use(express.static('public'))
-
-
 
 Server.listen(port, () => console.log(`Server is running on port ${port}`))
